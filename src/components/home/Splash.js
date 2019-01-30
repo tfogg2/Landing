c5e3aa7eee6a5f0f6f48c9e0a2124f186f22e1c7
Thomas Fogg <tfogg2@gmail.com>
2019-01-29 19:22:58 -0600

    React Landing Wireframe

---
+++ src/components/home/Splash.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class Splash extends Component{
+  render(){
+    return(
+      <div className="splash">
+
+      </div>
+    )
+  }
+}
+
+export default Splash;
